5671a1176ed358e511023b729452a0dcdbc26a1a
BigTree13 <956255917@qq.com>
2019-05-04 12:08:42 +0800

    数组中重复的数字（通过哈希表的方式）

--- Algorithm3/Algorithm3.cpp
+++ Algorithm3/Algorithm3.cpp
@@ -1,5 +1,6 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <map>
 #include "Sort.h"
 
 //思路一：先对数组进行排序，然后从前往后扫描，查找第一个重复的数字
@@ -71,6 +72,44 @@ bool sortFind(SortInfo info, int num[], int len, int* duplication)
 	return false;
 }
 
+//思路二：通过哈希表的方式，时间复杂度O(n)，空间复杂度O(n)
+bool hashFind(int num[], int len, int* duplication)
+{
+	if (num == nullptr || len <= 0)
+	{
+		return false;
+	}
+
+	for (int i = 0; i < len; i++)
+	{
+		if (num[i] > len - 1 || num[i] < 0)
+		{
+			return false;
+		}
+	}
+
+	std::map<int, int> regMap;
+	for (int i = 0; i < len; i++)
+	{
+		int flag = num[i];
+		if (regMap.count(flag))
+		{
+			if (regMap[flag] != 0)
+			{
+				*duplication = flag;
+				return true;
+			}
+		}
+		else
+		{
+			regMap[flag] = 0;
+			regMap[flag]++;
+		}
+	}
+
+	return false;
+}
+
 
 // ====================测试代码====================
 bool contains(int array[], int length, int number)
@@ -90,7 +129,9 @@ void test(const char* testName, int numbers[], int lengthNumbers, int expected[]
 	printf("%s begins: ", testName);
 
 	int duplication;
-	bool validInput = sortFind(HEAP, numbers, lengthNumbers, &duplication);
+
+	//bool validInput = sortFind(HEAP, numbers, lengthNumbers, &duplication);
+	bool validInput = hashFind(numbers, lengthNumbers, &duplication);
 
 	if (validArgument == validInput)
 	{
@@ -115,7 +156,6 @@ void test(const char* testName, int numbers[], int lengthNumbers, int expected[]
 		printf("FAILED.\n");
 	}
 }
-
 // 重复的数字是数组中最小的数字
 void test1()
 {
@@ -123,7 +163,6 @@ void test1()
 	int duplications[] = { 1 };
 	test("Test1", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), true);
 }
-
 // 重复的数字是数组中最大的数字
 void test2()
 {
@@ -131,7 +170,6 @@ void test2()
 	int duplications[] = { 4 };
 	test("Test2", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), true);
 }
-
 // 数组中存在多个重复的数字
 void test3()
 {
@@ -139,7 +177,6 @@ void test3()
 	int duplications[] = { 2, 4 };
 	test("Test3", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), true);
 }
-
 // 没有重复的数字
 void test4()
 {
@@ -147,7 +184,6 @@ void test4()
 	int duplications[] = { -1 }; // not in use in the test function
 	test("Test4", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), false);
 }
-
 // 没有重复的数字
 void test5()
 {
@@ -155,7 +191,6 @@ void test5()
 	int duplications[] = { -1 }; // not in use in the test function
 	test("Test5", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), false);
 }
-
 // 无效的输入
 void test6()
 {
