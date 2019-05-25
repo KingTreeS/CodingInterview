138a95fc9eea91cdc0c7a4882022d1c719128a6f
BigTree13 <956255917@qq.com>
2019-05-25 13:29:19 +0800

    矩阵中的路径

---
+++ Algorithm12/Algorithm12.cpp
@@ -0,0 +1,240 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stack>
+
+bool hasPathCore(const char* matrix, int rows, int cols, const char* str, int row, int col, int& pathLength, bool* visited)
+{
+	if (str[pathLength] == '\0')
+	{
+		return true;
+	}
+
+	bool hasPath = false;
+	if (row >= 0 && row < rows && col >= 0 && col < cols
+		&& matrix[row*cols + col] == str[pathLength]
+		&& !visited[row*cols + col])
+	{
+		pathLength++;
+		visited[row*cols + col] = true;
+
+		hasPath = hasPathCore(matrix, rows, cols, str, row, col + 1, pathLength, visited)
+			|| hasPathCore(matrix, rows, cols, str, row + 1, col, pathLength, visited)
+			|| hasPathCore(matrix, rows, cols, str, row, col - 1, pathLength, visited)
+			|| hasPathCore(matrix, rows, cols, str, row - 1, col, pathLength, visited);
+
+		if (!hasPath)
+		{
+			pathLength--;
+			visited[row*cols + col] = false;
+		}
+	}
+
+	return hasPath;
+}
+
+bool hasPath(const char* matrix, int rows, int cols, const char* str)
+{
+	if (matrix == nullptr || rows <= 0 || cols <= 0 || str == nullptr)
+	{
+		return false;
+	}
+
+	bool* visited = new bool[rows * cols];
+	memset(visited, false, rows*cols);
+	int pathLength = 0;
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			if (hasPathCore(matrix, rows, cols, str, i, j, pathLength, visited))
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+// ====================测试代码====================
+void Test(const char* testName, const char* matrix, int rows, int cols, const char* str, bool expected)
+{
+	if (testName != nullptr)
+		printf("%s begins: ", testName);
+
+	if (hasPath(matrix, rows, cols, str) == expected)
+		printf("Passed.\n");
+	else
+		printf("FAILED.\n");
+}
+
+//ABTG
+//CFCS
+//JDEH
+
+//BFCE
+void Test1()
+{
+	const char* matrix = "ABTGCFCSJDEH";
+	const char* str = "BFCE";
+
+	Test("Test1", (const char*)matrix, 3, 4, str, true);
+}
+
+//ABCE
+//SFCS
+//ADEE
+
+//SEE
+void Test2()
+{
+	const char* matrix = "ABCESFCSADEE";
+	const char* str = "SEE";
+
+	Test("Test2", (const char*)matrix, 3, 4, str, true);
+}
+
+//ABTG
+//CFCS
+//JDEH
+
+//ABFB
+void Test3()
+{
+	const char* matrix = "ABTGCFCSJDEH";
+	const char* str = "ABFB";
+
+	Test("Test3", (const char*)matrix, 3, 4, str, false);
+}
+
+//ABCEHJIG
+//SFCSLOPQ
+//ADEEMNOE
+//ADIDEJFM
+//VCEIFGGS
+
+//SLHECCEIDEJFGGFIE
+void Test4()
+{
+	const char* matrix = "ABCEHJIGSFCSLOPQADEEMNOEADIDEJFMVCEIFGGS";
+	const char* str = "SLHECCEIDEJFGGFIE";
+
+	Test("Test4", (const char*)matrix, 5, 8, str, true);
+}
+
+//ABCEHJIG
+//SFCSLOPQ
+//ADEEMNOE
+//ADIDEJFM
+//VCEIFGGS
+
+//SGGFIECVAASABCEHJIGQEM
+void Test5()
+{
+	const char* matrix = "ABCEHJIGSFCSLOPQADEEMNOEADIDEJFMVCEIFGGS";
+	const char* str = "SGGFIECVAASABCEHJIGQEM";
+
+	Test("Test5", (const char*)matrix, 5, 8, str, true);
+}
+
+//ABCEHJIG
+//SFCSLOPQ
+//ADEEMNOE
+//ADIDEJFM
+//VCEIFGGS
+
+//SGGFIECVAASABCEEJIGOEM
+void Test6()
+{
+	const char* matrix = "ABCEHJIGSFCSLOPQADEEMNOEADIDEJFMVCEIFGGS";
+	const char* str = "SGGFIECVAASABCEEJIGOEM";
+
+	Test("Test6", (const char*)matrix, 5, 8, str, false);
+}
+
+//ABCEHJIG
+//SFCSLOPQ
+//ADEEMNOE
+//ADIDEJFM
+//VCEIFGGS
+
+//SGGFIECVAASABCEHJIGQEMS
+void Test7()
+{
+	const char* matrix = "ABCEHJIGSFCSLOPQADEEMNOEADIDEJFMVCEIFGGS";
+	const char* str = "SGGFIECVAASABCEHJIGQEMS";
+
+	Test("Test7", (const char*)matrix, 5, 8, str, false);
+}
+
+//AAAA
+//AAAA
+//AAAA
+
+//AAAAAAAAAAAA
+void Test8()
+{
+	const char* matrix = "AAAAAAAAAAAA";
+	const char* str = "AAAAAAAAAAAA";
+
+	Test("Test8", (const char*)matrix, 3, 4, str, true);
+}
+
+//AAAA
+//AAAA
+//AAAA
+
+//AAAAAAAAAAAAA
+void Test9()
+{
+	const char* matrix = "AAAAAAAAAAAA";
+	const char* str = "AAAAAAAAAAAAA";
+
+	Test("Test9", (const char*)matrix, 3, 4, str, false);
+}
+
+//A
+
+//A
+void Test10()
+{
+	const char* matrix = "A";
+	const char* str = "A";
+
+	Test("Test10", (const char*)matrix, 1, 1, str, true);
+}
+
+//A
+
+//B
+void Test11()
+{
+	const char* matrix = "A";
+	const char* str = "B";
+
+	Test("Test11", (const char*)matrix, 1, 1, str, false);
+}
+
+void Test12()
+{
+	Test("Test12", nullptr, 0, 0, nullptr, false);
+}
+
+int main()
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+	Test7();
+	Test8();
+	Test9();
+	Test10();
+	Test11();
+	Test12();
+
+	return 0;
+}
