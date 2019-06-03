0c539655eac2dc70f5aab274e68cd7ae9d718e16
BigTree13 <956255917@qq.com>
2019-06-03 21:30:51 +0800

    二叉树的镜像

---
+++ Algorithm27/Algorithm27.cpp
@@ -0,0 +1,268 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stack>
+
+struct BinaryTreeNode
+{
+	int					m_nValue;
+	BinaryTreeNode*		m_pLeft;
+	BinaryTreeNode*		m_pRight;
+};
+
+BinaryTreeNode* CreateBinaryTreeNode(int value)
+{
+	BinaryTreeNode* pNode = new BinaryTreeNode();
+	pNode->m_nValue = value;
+	pNode->m_pLeft = nullptr;
+	pNode->m_pRight = nullptr;
+
+	return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pNode1, BinaryTreeNode* pNode2, BinaryTreeNode* pNode3)
+{
+	if (pNode1 == nullptr)
+	{
+		return;
+	}
+
+	pNode1->m_pLeft = pNode2;
+	pNode1->m_pRight = pNode3;
+}
+
+void PrintTree(BinaryTreeNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		return;
+	}
+
+	std::cout << "The root value is: " << pHead->m_nValue << std::endl;
+	if (pHead->m_pLeft != nullptr)
+	{
+		std::cout << "value of root's left child is: " << pHead->m_pLeft->m_nValue << std::endl;
+	}
+	else
+	{
+		std::cout << "left child is nullptr" << std::endl;
+	}
+	if (pHead->m_pRight != nullptr)
+	{
+		std::cout << "value of root's right child is: " << pHead->m_pRight->m_nValue << std::endl;
+	}
+	else
+	{
+		std::cout << "right child is nullptr" << std::endl;
+	}
+
+	PrintTree(pHead->m_pLeft);
+	PrintTree(pHead->m_pRight);
+}
+
+void DestroyTree(BinaryTreeNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		return;
+	}
+
+	BinaryTreeNode* pLeft = pHead->m_pLeft;
+	BinaryTreeNode* pRight = pHead->m_pRight;
+
+	delete pHead;
+	pHead = nullptr;
+
+	DestroyTree(pLeft);
+	DestroyTree(pRight);
+}
+
+//递归实现
+void MirrorRecursively(BinaryTreeNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		return;
+	}
+
+	BinaryTreeNode* tmp = pHead->m_pLeft;
+	pHead->m_pLeft = pHead->m_pRight;
+	pHead->m_pRight = tmp;
+
+	MirrorRecursively(pHead->m_pLeft);
+	MirrorRecursively(pHead->m_pRight);
+}
+
+//循环实现(借助栈实现)
+void MirrorIteratively(BinaryTreeNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		return;
+	}
+
+	std::stack<BinaryTreeNode*> stackBinaryTree;
+	stackBinaryTree.push(pHead);
+
+	while (!stackBinaryTree.empty())
+	{
+		BinaryTreeNode* pNode = stackBinaryTree.top();
+		stackBinaryTree.pop();
+
+		BinaryTreeNode* tmp = pNode->m_pLeft;
+		pNode->m_pLeft = pNode->m_pRight;
+		pNode->m_pRight = tmp;
+
+		if (pNode->m_pLeft != nullptr)
+		{
+			stackBinaryTree.push(pNode->m_pLeft);
+		}
+		if (pNode->m_pRight != nullptr)
+		{
+			stackBinaryTree.push(pNode->m_pRight);
+		}
+	}
+}
+
+// ====================测试代码====================
+// 测试完全二叉树：除了叶子节点，其他节点都有两个子节点
+//            8
+//        6      10
+//       5 7    9  11
+void Test1()
+{
+	printf("=====Test1 starts:=====\n");
+	BinaryTreeNode* pNode8 = CreateBinaryTreeNode(8);
+	BinaryTreeNode* pNode6 = CreateBinaryTreeNode(6);
+	BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+	BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+	BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+	BinaryTreeNode* pNode9 = CreateBinaryTreeNode(9);
+	BinaryTreeNode* pNode11 = CreateBinaryTreeNode(11);
+
+	ConnectTreeNodes(pNode8, pNode6, pNode10);
+	ConnectTreeNodes(pNode6, pNode5, pNode7);
+	ConnectTreeNodes(pNode10, pNode9, pNode11);
+
+	PrintTree(pNode8);
+
+	printf("=====Test1: MirrorRecursively=====\n");
+	MirrorRecursively(pNode8);
+	PrintTree(pNode8);
+
+	printf("=====Test1: MirrorIteratively=====\n");
+	MirrorIteratively(pNode8);
+	PrintTree(pNode8);
+
+	DestroyTree(pNode8);
+}
+
+// 测试二叉树：出叶子结点之外，左右的结点都有且只有一个左子结点
+//            8
+//          7   
+//        6 
+//      5
+//    4
+void Test2()
+{
+	printf("=====Test2 starts:=====\n");
+	BinaryTreeNode* pNode8 = CreateBinaryTreeNode(8);
+	BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+	BinaryTreeNode* pNode6 = CreateBinaryTreeNode(6);
+	BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+	BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+
+	ConnectTreeNodes(pNode8, pNode7, nullptr);
+	ConnectTreeNodes(pNode7, pNode6, nullptr);
+	ConnectTreeNodes(pNode6, pNode5, nullptr);
+	ConnectTreeNodes(pNode5, pNode4, nullptr);
+
+	PrintTree(pNode8);
+
+	printf("=====Test2: MirrorRecursively=====\n");
+	MirrorRecursively(pNode8);
+	PrintTree(pNode8);
+
+	printf("=====Test2: MirrorIteratively=====\n");
+	MirrorIteratively(pNode8);
+	PrintTree(pNode8);
+
+	DestroyTree(pNode8);
+}
+
+// 测试二叉树：出叶子结点之外，左右的结点都有且只有一个右子结点
+//            8
+//             7   
+//              6 
+//               5
+//                4
+void Test3()
+{
+	printf("=====Test3 starts:=====\n");
+	BinaryTreeNode* pNode8 = CreateBinaryTreeNode(8);
+	BinaryTreeNode* pNode7 = CreateBinaryTreeNode(7);
+	BinaryTreeNode* pNode6 = CreateBinaryTreeNode(6);
+	BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+	BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+
+	ConnectTreeNodes(pNode8, nullptr, pNode7);
+	ConnectTreeNodes(pNode7, nullptr, pNode6);
+	ConnectTreeNodes(pNode6, nullptr, pNode5);
+	ConnectTreeNodes(pNode5, nullptr, pNode4);
+
+	PrintTree(pNode8);
+
+	printf("=====Test3: MirrorRecursively=====\n");
+	MirrorRecursively(pNode8);
+	PrintTree(pNode8);
+
+	printf("=====Test3: MirrorIteratively=====\n");
+	MirrorIteratively(pNode8);
+	PrintTree(pNode8);
+
+	DestroyTree(pNode8);
+}
+
+// 测试空二叉树：根结点为空指针
+void Test4()
+{
+	printf("=====Test4 starts:=====\n");
+	BinaryTreeNode* pNode = nullptr;
+
+	PrintTree(pNode);
+
+	printf("=====Test4: MirrorRecursively=====\n");
+	MirrorRecursively(pNode);
+	PrintTree(pNode);
+
+	printf("=====Test4: MirrorIteratively=====\n");
+	MirrorIteratively(pNode);
+	PrintTree(pNode);
+}
+
+// 测试只有一个结点的二叉树
+void Test5()
+{
+	printf("=====Test5 starts:=====\n");
+	BinaryTreeNode* pNode8 = CreateBinaryTreeNode(8);
+
+	PrintTree(pNode8);
+
+	printf("=====Test4: MirrorRecursively=====\n");
+	MirrorRecursively(pNode8);
+	PrintTree(pNode8);
+
+	printf("=====Test4: MirrorIteratively=====\n");
+	MirrorIteratively(pNode8);
+	PrintTree(pNode8);
+}
+
+int main()
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+
+	return 0;
+}
