6383c46737500179e3dd349b6ea844fe1c3e98ba
BigTree13 <956255917@qq.com>
2019-06-03 16:05:56 +0800

    链表中倒数第k个节点

---
+++ Algorithm22/Algorithm22.cpp
@@ -0,0 +1,214 @@
+﻿#include "pch.h"
+#include <iostream>
+
+struct ListNode
+{
+	int			m_nValue;
+	ListNode*	m_pNext;
+};
+
+ListNode* CreateListNode(int value)
+{
+	ListNode* pNode = new ListNode();
+	pNode->m_nValue = value;
+	pNode->m_pNext = nullptr;
+
+	return pNode;
+}
+
+void ConnectListNodes(ListNode* pNode1, ListNode* pNode2)
+{
+	if (pNode1 == nullptr)
+	{
+		return;
+	}
+
+	pNode1->m_pNext = pNode2;
+}
+
+void PrintListNode(ListNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		std::cout << "nullptr" << std::endl;
+		return;
+	}
+
+	std::cout << pHead->m_nValue << std::endl;
+}
+
+void DestroyList(ListNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		return;
+	}
+
+	ListNode* pNext = pHead->m_pNext;
+	DestroyList(pNext);
+
+	delete pHead;
+	pHead = nullptr;
+}
+
+ListNode* FindKthToTail(ListNode* pHead, int k)
+{
+	if (pHead == nullptr || k < 1)
+	{
+		return nullptr;
+	}
+
+	ListNode* ptr1 = pHead;
+	ListNode* ptr2 = pHead;
+
+	while (k > 0 && ptr1 != nullptr)
+	{
+		ptr1 = ptr1->m_pNext;
+		k--;
+	}
+
+	if (k != 0)
+	{
+		return nullptr;
+	}
+
+	while (ptr1 != nullptr)
+	{
+		ptr1 = ptr1->m_pNext;
+		ptr2 = ptr2->m_pNext;
+	}
+
+	return ptr2;
+}
+
+// ====================测试代码====================
+// 测试要找的结点在链表中间
+void Test1()
+{
+	printf("=====Test1 starts:=====\n");
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	printf("expected result: 4.\n");
+	ListNode* pNode = FindKthToTail(pNode1, 2);
+	PrintListNode(pNode);
+
+	DestroyList(pNode1);
+}
+
+// 测试要找的结点是链表的尾结点
+void Test2()
+{
+	printf("=====Test2 starts:=====\n");
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	printf("expected result: 5.\n");
+	ListNode* pNode = FindKthToTail(pNode1, 1);
+	PrintListNode(pNode);
+
+	DestroyList(pNode1);
+}
+
+// 测试要找的结点是链表的头结点
+void Test3()
+{
+	printf("=====Test3 starts:=====\n");
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	printf("expected result: 1.\n");
+	ListNode* pNode = FindKthToTail(pNode1, 5);
+	PrintListNode(pNode);
+
+	DestroyList(pNode1);
+}
+
+// 测试空链表
+void Test4()
+{
+	printf("=====Test4 starts:=====\n");
+	printf("expected result: nullptr.\n");
+	ListNode* pNode = FindKthToTail(nullptr, 100);
+	PrintListNode(pNode);
+}
+
+// 测试输入的第二个参数大于链表的结点总数
+void Test5()
+{
+	printf("=====Test5 starts:=====\n");
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	printf("expected result: nullptr.\n");
+	ListNode* pNode = FindKthToTail(pNode1, 6);
+	PrintListNode(pNode);
+
+	DestroyList(pNode1);
+}
+
+// 测试输入的第二个参数为0
+void Test6()
+{
+	printf("=====Test6 starts:=====\n");
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	printf("expected result: nullptr.\n");
+	ListNode* pNode = FindKthToTail(pNode1, 0);
+	PrintListNode(pNode);
+
+	DestroyList(pNode1);
+}
+
+int main()
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+
+	return 0;
+}
