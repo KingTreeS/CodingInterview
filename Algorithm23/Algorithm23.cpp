e90d625a26d2b2d1df9ccff9c2f621461aa5e2cb
BigTree13 <956255917@qq.com>
2019-06-03 17:05:41 +0800

    链表中环的入口

---
+++ Algorithm23/Algorithm23.cpp
@@ -0,0 +1,252 @@
+﻿#include "pch.h"
+#include <iostream>
+
+struct ListNode
+{
+	int			m_nValue;
+	ListNode*	m_pNext;
+};
+
+ListNode* CreateListNode(int value)
+{
+	ListNode* pNode = new ListNode();
+	pNode->m_nValue = value;
+	pNode->m_pNext = nullptr;
+
+	return pNode;
+}
+
+void ConnectListNodes(ListNode* pNode1, ListNode* pNode2)
+{
+	if (pNode1 == nullptr)
+	{
+		return;
+	}
+
+	pNode1->m_pNext = pNode2;
+}
+
+void DestroyList(ListNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		return;
+	}
+
+	ListNode* pNode = pHead->m_pNext;
+	DestroyList(pNode);
+
+	delete pHead;
+	pHead = nullptr;
+}
+
+ListNode* EntryNodeOfLoop(ListNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		return nullptr;
+	}
+
+	ListNode* ptr1 = pHead;
+	ListNode* ptr2 = pHead;
+
+	while (ptr1 != nullptr && ptr1->m_pNext != nullptr)
+	{
+		ptr1 = ptr1->m_pNext->m_pNext;
+		ptr2 = ptr2->m_pNext;
+
+		if (ptr1 == ptr2)
+		{
+			break;
+		}
+	}
+
+	if (ptr1 == nullptr || ptr1->m_pNext == nullptr)
+	{
+		return nullptr;
+	}
+
+	int loopCount = 1;
+	ptr1 = ptr1->m_pNext;
+	while (ptr1 != ptr2)
+	{
+		loopCount++;
+		ptr1 = ptr1->m_pNext;
+	}
+
+	ptr1 = pHead;
+	ptr2 = pHead;
+
+	while (loopCount > 0)
+	{
+		ptr1 = ptr1->m_pNext;
+		loopCount--;
+	}
+
+	while (ptr1 != ptr2)
+	{
+		ptr1 = ptr1->m_pNext;
+		ptr2 = ptr2->m_pNext;
+	}
+
+	return ptr1;
+}
+
+// ==================== Test Code ====================
+void Test(const char* testName, ListNode* pHead, ListNode* entryNode)
+{
+	if (testName != nullptr)
+		printf("%s begins: ", testName);
+
+	if (EntryNodeOfLoop(pHead) == entryNode)
+		printf("Passed.\n");
+	else
+		printf("FAILED.\n");
+}
+
+// A list has a node, without a loop
+void Test1()
+{
+	ListNode* pNode1 = CreateListNode(1);
+
+	Test("Test1", pNode1, nullptr);
+
+	DestroyList(pNode1);
+}
+
+// A list has a node, with a loop
+void Test2()
+{
+	ListNode* pNode1 = CreateListNode(1);
+	ConnectListNodes(pNode1, pNode1);
+
+	Test("Test2", pNode1, pNode1);
+
+	delete pNode1;
+	pNode1 = nullptr;
+}
+
+// A list has multiple nodes, with a loop 
+void Test3()
+{
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+	ConnectListNodes(pNode5, pNode3);
+
+	Test("Test3", pNode1, pNode3);
+
+	delete pNode1;
+	pNode1 = nullptr;
+	delete pNode2;
+	pNode2 = nullptr;
+	delete pNode3;
+	pNode3 = nullptr;
+	delete pNode4;
+	pNode4 = nullptr;
+	delete pNode5;
+	pNode5 = nullptr;
+}
+
+// A list has multiple nodes, with a loop 
+void Test4()
+{
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+	ConnectListNodes(pNode5, pNode1);
+
+	Test("Test4", pNode1, pNode1);
+
+	delete pNode1;
+	pNode1 = nullptr;
+	delete pNode2;
+	pNode2 = nullptr;
+	delete pNode3;
+	pNode3 = nullptr;
+	delete pNode4;
+	pNode4 = nullptr;
+	delete pNode5;
+	pNode5 = nullptr;
+}
+
+// A list has multiple nodes, with a loop 
+void Test5()
+{
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+	ConnectListNodes(pNode5, pNode5);
+
+	Test("Test5", pNode1, pNode5);
+
+	delete pNode1;
+	pNode1 = nullptr;
+	delete pNode2;
+	pNode2 = nullptr;
+	delete pNode3;
+	pNode3 = nullptr;
+	delete pNode4;
+	pNode4 = nullptr;
+	delete pNode5;
+	pNode5 = nullptr;
+}
+
+// A list has multiple nodes, without a loop 
+void Test6()
+{
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	Test("Test6", pNode1, nullptr);
+
+	DestroyList(pNode1);
+}
+
+// Empty list
+void Test7()
+{
+	Test("Test7", nullptr, nullptr);
+}
+
+int main()
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+	Test7();
+
+	return 0;
+}
