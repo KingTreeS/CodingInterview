3a8a1f4c414b091d1c04d5e6d83ab3c006c43bde
BigTree13 <956255917@qq.com>
2019-06-04 18:33:11 +0800

    复杂链表的复制

---
+++ Algorithm35/Algorithm35.cpp
@@ -0,0 +1,220 @@
+﻿#include "pch.h"
+#include <iostream>
+
+struct ComplexListNode
+{
+	int m_nValue;
+	ComplexListNode* m_pNext;
+	ComplexListNode* m_pSibling;
+};
+
+ComplexListNode* CreateNode(int value)
+{
+	ComplexListNode* pNode = new ComplexListNode();
+	pNode->m_nValue = value;
+	pNode->m_pNext = nullptr;
+	pNode->m_pSibling = nullptr;
+
+	return pNode;
+}
+
+void BuildNodes(ComplexListNode* pNode1, ComplexListNode* pNode2, ComplexListNode* pNode3)
+{
+	if (pNode1 == nullptr)
+	{
+		return;
+	}
+
+	pNode1->m_pNext = pNode2;
+	pNode1->m_pSibling = pNode3;
+}
+
+void PrintList(ComplexListNode* pHead)
+{
+	ComplexListNode* pNode = pHead;
+	while (pNode != nullptr)
+	{
+		printf("The value of this node is: %d.\n", pNode->m_nValue);
+
+		if (pNode->m_pSibling != nullptr)
+			printf("The value of its sibling is: %d.\n", pNode->m_pSibling->m_nValue);
+		else
+			printf("This node does not have a sibling.\n");
+
+		printf("\n");
+
+		pNode = pNode->m_pNext;
+	}
+}
+
+void CloneNodes(ComplexListNode* pHead)
+{
+	ComplexListNode* pNode = pHead;
+	while (pNode != nullptr)
+	{
+		ComplexListNode* pCloneNode = new ComplexListNode();
+		pCloneNode->m_nValue = pNode->m_nValue;
+		pCloneNode->m_pNext = pNode->m_pNext;
+		pCloneNode->m_pSibling = nullptr;
+
+		pNode->m_pNext = pCloneNode;
+		pNode = pCloneNode->m_pNext;
+	}
+}
+
+void ConnectSiblingNodes(ComplexListNode* pHead)
+{
+	ComplexListNode* pNode = pHead;
+	while (pNode != nullptr)
+	{
+		pNode->m_pNext->m_pSibling = pNode->m_pSibling == nullptr ? nullptr : pNode->m_pSibling->m_pNext;
+		pNode = pNode->m_pNext->m_pNext;
+	}
+}
+
+ComplexListNode* SplitNodes(ComplexListNode* pHead)
+{
+	ComplexListNode* pNode1 = pHead;
+	ComplexListNode* pNode2 = pHead->m_pNext;
+	ComplexListNode* pHead2 = pNode2;
+
+
+	while (pNode1 != nullptr)
+	{
+		pNode1->m_pNext = pNode2->m_pNext;
+		if (pNode1->m_pNext == nullptr)
+		{
+			pNode2->m_pNext = nullptr;
+		}
+		else
+		{
+			pNode2->m_pNext = pNode1->m_pNext->m_pNext;
+		}
+
+		pNode1 = pNode1->m_pNext;
+		pNode2 = pNode2->m_pNext;
+	}
+
+	return pHead2;
+}
+
+ComplexListNode* Clone(ComplexListNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		return nullptr;
+	}
+
+	CloneNodes(pHead);
+	ConnectSiblingNodes(pHead);
+	return SplitNodes(pHead);
+}
+
+// ====================测试代码====================
+void Test(const char* testName, ComplexListNode* pHead)
+{
+	if (testName != nullptr)
+		printf("%s begins:\n", testName);
+
+	printf("The original list is:\n");
+	PrintList(pHead);
+
+	ComplexListNode* pClonedHead = Clone(pHead);
+
+	printf("The cloned list is:\n");
+	PrintList(pClonedHead);
+}
+
+//          -----------------
+//         \|/              |
+//  1-------2-------3-------4-------5
+//  |       |      /|\             /|\
+//  --------+--------               |
+//          -------------------------
+void Test1()
+{
+	ComplexListNode* pNode1 = CreateNode(1);
+	ComplexListNode* pNode2 = CreateNode(2);
+	ComplexListNode* pNode3 = CreateNode(3);
+	ComplexListNode* pNode4 = CreateNode(4);
+	ComplexListNode* pNode5 = CreateNode(5);
+
+	BuildNodes(pNode1, pNode2, pNode3);
+	BuildNodes(pNode2, pNode3, pNode5);
+	BuildNodes(pNode3, pNode4, nullptr);
+	BuildNodes(pNode4, pNode5, pNode2);
+
+	Test("Test1", pNode1);
+}
+
+// m_pSibling指向结点自身
+//          -----------------
+//         \|/              |
+//  1-------2-------3-------4-------5
+//         |       | /|\           /|\
+//         |       | --             |
+//         |------------------------|
+void Test2()
+{
+	ComplexListNode* pNode1 = CreateNode(1);
+	ComplexListNode* pNode2 = CreateNode(2);
+	ComplexListNode* pNode3 = CreateNode(3);
+	ComplexListNode* pNode4 = CreateNode(4);
+	ComplexListNode* pNode5 = CreateNode(5);
+
+	BuildNodes(pNode1, pNode2, nullptr);
+	BuildNodes(pNode2, pNode3, pNode5);
+	BuildNodes(pNode3, pNode4, pNode3);
+	BuildNodes(pNode4, pNode5, pNode2);
+
+	Test("Test2", pNode1);
+}
+
+// m_pSibling形成环
+//          -----------------
+//         \|/              |
+//  1-------2-------3-------4-------5
+//          |              /|\
+//          |               |
+//          |---------------|
+void Test3()
+{
+	ComplexListNode* pNode1 = CreateNode(1);
+	ComplexListNode* pNode2 = CreateNode(2);
+	ComplexListNode* pNode3 = CreateNode(3);
+	ComplexListNode* pNode4 = CreateNode(4);
+	ComplexListNode* pNode5 = CreateNode(5);
+
+	BuildNodes(pNode1, pNode2, nullptr);
+	BuildNodes(pNode2, pNode3, pNode4);
+	BuildNodes(pNode3, pNode4, nullptr);
+	BuildNodes(pNode4, pNode5, pNode2);
+
+	Test("Test3", pNode1);
+}
+
+// 只有一个结点
+void Test4()
+{
+	ComplexListNode* pNode1 = CreateNode(1);
+	BuildNodes(pNode1, nullptr, pNode1);
+
+	Test("Test4", pNode1);
+}
+
+// 鲁棒性测试
+void Test5()
+{
+	Test("Test5", nullptr);
+}
+
+int main()
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+
+	return 0;
+}
