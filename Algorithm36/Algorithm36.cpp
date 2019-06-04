3ff6fbdfcb9d91f35c07fb32366a344d84b7be5b
BigTree13 <956255917@qq.com>
2019-06-04 19:51:31 +0800

    二叉搜索树与双向链表

---
+++ Algorithm36/Algorithm36.cpp
@@ -0,0 +1,255 @@
+﻿#include "pch.h"
+#include <iostream>
+
+struct BinaryTreeNode
+{
+	int					m_nValue;
+	BinaryTreeNode*		m_pLeft;
+	BinaryTreeNode*		m_pRight;
+};
+
+BinaryTreeNode* CreateBinaryTreeNode(int value)
+{
+	BinaryTreeNode* pNode = new BinaryTreeNode();
+	pNode->m_nValue = value;
+	pNode->m_pLeft = nullptr;
+	pNode->m_pRight = nullptr;
+
+	return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pNode1, BinaryTreeNode* pNode2, BinaryTreeNode* pNode3)
+{
+	if (pNode1 == nullptr)
+	{
+		return;
+	}
+
+	pNode1->m_pLeft = pNode2;
+	pNode1->m_pRight = pNode3;
+}
+
+void PrintTree(BinaryTreeNode* pHead)
+{
+	if (pHead == nullptr)
+	{
+		return;
+	}
+
+	std::cout << "The root value is: " << pHead->m_nValue << std::endl;
+	if (pHead->m_pLeft != nullptr)
+	{
+		std::cout << "value of root's left child is: " << pHead->m_pLeft->m_nValue << std::endl;
+	}
+	else
+	{
+		std::cout << "left child is nullptr" << std::endl;
+	}
+	if (pHead->m_pRight != nullptr)
+	{
+		std::cout << "value of root's right child is: " << pHead->m_pRight->m_nValue << std::endl;
+	}
+	else
+	{
+		std::cout << "right child is nullptr" << std::endl;
+	}
+
+	PrintTree(pHead->m_pLeft);
+	PrintTree(pHead->m_pRight);
+}
+
+void ConvertCore(BinaryTreeNode* pRoot, BinaryTreeNode** pEndNodeOfConvert)
+{
+	BinaryTreeNode* pCurrent = pRoot;
+	if (pCurrent->m_pLeft != nullptr)
+	{
+		ConvertCore(pCurrent->m_pLeft, pEndNodeOfConvert);
+	}
+
+	pCurrent->m_pLeft = *pEndNodeOfConvert;
+	if (*pEndNodeOfConvert != nullptr)
+	{
+		(*pEndNodeOfConvert)->m_pRight = pCurrent;
+	}
+	*pEndNodeOfConvert = pCurrent;
+
+	if (pCurrent->m_pRight != nullptr)
+	{
+		ConvertCore(pCurrent->m_pRight, pEndNodeOfConvert);
+	}
+}
+
+BinaryTreeNode* Convert(BinaryTreeNode* pRoot)
+{
+	if (pRoot == nullptr)
+	{
+		return nullptr;
+	}
+
+	BinaryTreeNode* pEndNodeOfConvert = nullptr;
+	ConvertCore(pRoot, &pEndNodeOfConvert);
+
+	BinaryTreeNode* pHead = pEndNodeOfConvert;
+	while (pHead->m_pLeft != nullptr)
+	{
+		pHead = pHead->m_pLeft;
+	}
+
+	return pHead;
+}
+
+// ====================测试代码====================
+void PrintDoubleLinkedList(BinaryTreeNode* pHeadOfList)
+{
+	BinaryTreeNode* pNode = pHeadOfList;
+
+	printf("The nodes from left to right are:\n");
+	while (pNode != nullptr)
+	{
+		printf("%d\t", pNode->m_nValue);
+
+		if (pNode->m_pRight == nullptr)
+			break;
+		pNode = pNode->m_pRight;
+	}
+
+	printf("\nThe nodes from right to left are:\n");
+	while (pNode != nullptr)
+	{
+		printf("%d\t", pNode->m_nValue);
+
+		if (pNode->m_pLeft == nullptr)
+			break;
+		pNode = pNode->m_pLeft;
+	}
+
+	printf("\n");
+}
+
+void DestroyList(BinaryTreeNode* pHeadOfList)
+{
+	BinaryTreeNode* pNode = pHeadOfList;
+	while (pNode != nullptr)
+	{
+		BinaryTreeNode* pNext = pNode->m_pRight;
+
+		delete pNode;
+		pNode = pNext;
+	}
+}
+
+void Test(const char* testName, BinaryTreeNode* pRootOfTree)
+{
+	if (testName != nullptr)
+		printf("%s begins:\n", testName);
+
+	PrintTree(pRootOfTree);
+
+	BinaryTreeNode* pHeadOfList = Convert(pRootOfTree);
+
+	PrintDoubleLinkedList(pHeadOfList);
+}
+
+//            10
+//         /      \
+//        6        14
+//       /\        /\
+//      4  8     12  16
+void Test1()
+{
+	BinaryTreeNode* pNode10 = CreateBinaryTreeNode(10);
+	BinaryTreeNode* pNode6 = CreateBinaryTreeNode(6);
+	BinaryTreeNode* pNode14 = CreateBinaryTreeNode(14);
+	BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+	BinaryTreeNode* pNode8 = CreateBinaryTreeNode(8);
+	BinaryTreeNode* pNode12 = CreateBinaryTreeNode(12);
+	BinaryTreeNode* pNode16 = CreateBinaryTreeNode(16);
+
+	ConnectTreeNodes(pNode10, pNode6, pNode14);
+	ConnectTreeNodes(pNode6, pNode4, pNode8);
+	ConnectTreeNodes(pNode14, pNode12, pNode16);
+
+	Test("Test1", pNode10);
+
+	DestroyList(pNode4);
+}
+
+//               5
+//              /
+//             4
+//            /
+//           3
+//          /
+//         2
+//        /
+//       1
+void Test2()
+{
+	BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+	BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+	BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+	BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+	BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+
+	ConnectTreeNodes(pNode5, pNode4, nullptr);
+	ConnectTreeNodes(pNode4, pNode3, nullptr);
+	ConnectTreeNodes(pNode3, pNode2, nullptr);
+	ConnectTreeNodes(pNode2, pNode1, nullptr);
+
+	Test("Test2", pNode5);
+
+	DestroyList(pNode1);
+}
+
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+void Test3()
+{
+	BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+	BinaryTreeNode* pNode2 = CreateBinaryTreeNode(2);
+	BinaryTreeNode* pNode3 = CreateBinaryTreeNode(3);
+	BinaryTreeNode* pNode4 = CreateBinaryTreeNode(4);
+	BinaryTreeNode* pNode5 = CreateBinaryTreeNode(5);
+
+	ConnectTreeNodes(pNode1, nullptr, pNode2);
+	ConnectTreeNodes(pNode2, nullptr, pNode3);
+	ConnectTreeNodes(pNode3, nullptr, pNode4);
+	ConnectTreeNodes(pNode4, nullptr, pNode5);
+
+	Test("Test3", pNode1);
+
+	DestroyList(pNode1);
+}
+
+// 树中只有1个结点
+void Test4()
+{
+	BinaryTreeNode* pNode1 = CreateBinaryTreeNode(1);
+	Test("Test4", pNode1);
+
+	DestroyList(pNode1);
+}
+
+// 树中没有结点
+void Test5()
+{
+	Test("Test5", nullptr);
+}
+
+int main()
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+
+	return 0;
+}
