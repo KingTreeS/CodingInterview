24134f42b0aed7b61b1c15d0fd7f6fe906ca8dec
BigTree13 <956255917@qq.com>
2019-06-08 13:33:08 +0800

    字符串的排列组合

--- Algorithm38/Algorithm38.cpp
+++ Algorithm38/Algorithm38.cpp
@@ -1,6 +1,8 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <vector>
 
+//排列问题
 void PermutationCore(char* pStr, char* pStart)
 {
 	if (*pStart == '\0')
@@ -38,6 +40,54 @@ void Permutation(char* pStr)
 	PermutationCore(pStr, pStr);
 }
 
+//组合问题
+void CombineCore(char* pStr, int m, std::vector<char*>& m_vectorList)
+{
+	if (m == 0)
+	{
+		for (auto character : m_vectorList)
+		{
+			std::cout << *character;
+		}
+		std::cout << "、";
+		return;
+	}
+
+	if (*pStr != '\0')
+	{
+		CombineCore(pStr + 1, m, m_vectorList);
+
+		m_vectorList.push_back(pStr);
+		CombineCore(pStr + 1, m-1, m_vectorList);
+		m_vectorList.pop_back();
+	}
+
+	return;
+}
+
+void Combine(char* pStr)
+{
+	if (pStr == nullptr)
+	{
+		return;
+	}
+
+	char* pStart = pStr;
+	int length = 0;
+	while (*pStr != '\0')
+	{
+		length++;
+		pStr++;
+	}
+
+	std::vector<char*> m_vectorList;
+
+	for (int i = 1; i <= length; i++)
+	{
+		CombineCore(pStart, i, m_vectorList);
+	}
+}
+
 // ====================测试代码====================
 void Test(char* pStr)
 {
@@ -46,7 +96,11 @@ void Test(char* pStr)
 	else
 		printf("Test for %s begins:\n", pStr);
 
+	std::cout << "排列情况："<<"\n";
 	Permutation(pStr);
+
+	std::cout << "\n" << "组合情况：" << "\n";
+	Combine(pStr);
 
 	printf("\n");
 }
